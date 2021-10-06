b4cd06c850395d6eee48a6e28806e6cc42a12194
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-10-06 17:52:29 +0200

    Added interface for login and register

---
+++ Recovery/Recovery_Backend_Data/Interfaces/IUserContext.cs
@@ -0,0 +1,14 @@
+﻿using Recovery_Backend_Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recovery_Backend_Data.Interfaces
+{
+    public interface IUserContext
+    {
+        UserDTO RegisterUser(string Fname, string Lname, string email, string password, string birthdate);
+        UserDTO LoginUser(string email, string password);
+        UserDTO GetUserDetails(string unique_id);
+    }
+}
